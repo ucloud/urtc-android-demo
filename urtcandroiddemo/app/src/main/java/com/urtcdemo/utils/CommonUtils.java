aaf3ebcaccd608e1db2147db88de20e2fb21b680
wuhaotianciel <ciel.wu@ucloud.cn>
2020-12-09 10:17:00 +0800

    1.临时提交

--- urtcandroiddemo/app/src/main/java/com/urtcdemo/utils/CommonUtils.java
+++ urtcandroiddemo/app/src/main/java/com/urtcdemo/utils/CommonUtils.java
@@ -16,15 +16,32 @@ public class CommonUtils {
     public static final int AUTO_MODE = 0;
     public static final int MANUAL_MODE = 1;
 
+    public static final boolean CAMERA_ON = true;
+    public static final boolean SCREEN_OFF = false;
+    public static final boolean MIC_ON = true;
+
+    public static final int nv21_format = 1;
+    public static final int nv12_format = 2;
+    public static final int i420_format = 3;
+    public static final int rgba_format = 4;
+    public static final int argb_format = 5;
+    public static final int rgb24_format = 6;
+    public static final int rgb565_format = 7;
+
     public static final String videoprofile = "videoprofile";
     public static final String capture_mode = "capturemode";
+    public static final String CAMERA_ENABLE = "CAMERA_ENABLE";
+    public static final String MIC_ENABLE = "MIC_ENABLE";
+    public static final String SCREEN_ENABLE = "SCREEN_ENABLE";
     public static final String PUBLISH_MODE = "PUBLISH_MODE";
-    public static final String SCRIBE_MODE = "SCRIBE_MODE";
+    public static final String SUBSCRIBE_MODE = "SUBSCRIBE_MODE";
     public static final String SDK_STREAM_ROLE = "SDK_STREAM_ROLE";
     public static final String SDK_CLASS_TYPE = "SDK_CLASS_TYPE";
     public static final String SDK_SUPPORT_MIX = "SDK_SUPPORT_MIX";
     public static final String SDK_IS_LOOP = "SDK_IS_LOOP";
     public static final String SDK_MIX_FILE_PATH = "SDK_MIX_FILE_PATH";
+    public static final String CAMERA_CAPTURE_MODE = "CAMERA_CAPTURE_MODE";
+    public static final String EXTEND_CAMERA_VIDEO_FORMAT = "EXTEND_CAMERA_VIDEO_FORMAT";
 
     public static final String RTSP_URL_KEY = "RTSP_URL_TEXT" ;
     public static final String RTST_URL = "rtsp://192.168.161.148:554/ch1";
@@ -34,9 +51,6 @@ public class CommonUtils {
     public static final String APPID_KEY = "APPID_KEY";
     public static final String APP_ID = "URtc-h4r1txxy";
     public static final String SEC_KEY = "9129304dbf8c5c4bf68d70824462409f";
-
-//    public static final String APP_ID = "urtc-mxr2seet";
-//    public static final String SEC_KEY = "f48b1dd672ed41ee9101100bc091304d";
 
 
     @IntDef({AUTO_MODE, MANUAL_MODE})
